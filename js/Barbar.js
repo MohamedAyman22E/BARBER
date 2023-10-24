8cd019733b6e6b11397f019efd09ce63c2937f62
moAymanEz <moAyman@gmail.com>
2023-10-25 00:35:40 +0300

    25-10-2023 12:35AM

--- js/Barbar.js
+++ js/Barbar.js
@@ -10,3 +10,29 @@ let header_bottom = document.querySelector(".header_bottom");
 window.addEventListener("scroll", () => {
   header_bottom.classList.toggle("active", scrollY > 120);
 });
+
+// pricing
+let allButtonBricing = document.querySelectorAll(".button button");
+let allBricingBoxs = document.querySelectorAll(".allBricingBoxs .aboutBox");
+
+allButtonBricing.forEach(e => {
+  e.addEventListener("click", () => {
+    allButtonBricing.forEach(ele => {
+      ele.classList.remove("active");
+    });
+    e.classList.add("active");
+    let filter = e.getAttribute("data-filter");
+    allBricingBoxs.forEach(index => {
+      if (filter == index.getAttribute("data-category")) {
+        index.classList.add("show");
+        index.classList.remove("hide");
+      } else {
+        index.classList.remove("show");
+        index.classList.add("hide");
+      }
+      if (filter === "all") {
+        index.classList.add("show");
+      }
+    });
+  });
+});
